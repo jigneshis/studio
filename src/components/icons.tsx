903ee15f14e60330c9bc8484815bbfd9abe48073
JIGNESH Wadhwani (JIGNESHIS) <jigneshwadhwani692@gmail.com>
2025-07-18 04:20:31 +0000

    change the logo of the app from the currnent to - https://qovhpsmldsalfn

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,21 +1,14 @@
+import type { HTMLAttributes } from 'react';
 import type { SVGProps } from "react";
 
-export function RenderriLogo(props: SVGProps<SVGSVGElement>) {
+export function RenderriLogo(props: HTMLAttributes<HTMLImageElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src="https://qovhpsmldsalfnlcalnv.supabase.co/storage/v1/object/public/main//Logo.png"
+      alt="Renderri Logo"
       {...props}
-    >
-      <path d="M12 2L2 7l10 5 10-5-10-5z" />
-      <path d="M2 17l10 5 10-5" />
-      <path d="M2 12l10 5 10-5" />
-    </svg>
+    />
   );
 }
 
